fa15eb0efdfc2bd8cc605740ef9741acb938d684
IT26101426 <it26101426@my.sliit.lk>
2026-09-18 15:20:42 +0530

    IT26101426Lab8Q1.java

---
+++ Q1/IT26101426Lab8Q1B.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class IT26101426Lab8Q1B{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] myArray = new int[5];
+        int[] evenArray = new int[5];
+
+        System.out.println("Enter 5 Numbers:");
+        for(int i=0; i<5; i++){
+            System.out.print("Enter Number "+(i+1)+": ");
+            myArray[i]=sc.nextInt();
+        }
+
+        int j=0;
+        for(int i=0; i<5; i++){
+            if(myArray[i]%2==0){
+                evenArray[j]=myArray[i];
+                j++;
+            }
+        }
+
+        System.out.println("\nmyArray Contents:");
+        for(int i=0; i<5; i++){
+            System.out.print(myArray[i]+" ");
+        }
+
+        System.out.println("\n\nevenArray Contents:");
+        for(int i=0; i<5; i++){
+            System.out.print(evenArray[i]+" ");
+        }
+    }
+}
